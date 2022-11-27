d5f5bf4e4e4d961f8cb8ff7751b9e526fdf80d4f
Emrikcah <curryandr3@gmail.com>
2022-11-27 14:52:21 -0500

    image fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({name,population,region,subregion,flags}) => {
 
   return (
     <article className='max-w-[264px] bg-white  dark:bg-dark-mode-elements rounded-lg shadow overflow-hidden'>
-      <img src={flags.svg} alt={`${name.common} flag`} />
+      <img className='w-full h-40 object-cover ' src={flags.svg} alt={`${name.common} flag`} />
       <div className='pt-6 pb-11 pl-6 '>
         <h2 className='font-extrabold text-light-mode-text dark:text-white text-lg pb-4'>{name.common}</h2>
         <ul className='space-y-2'>
