a2506362b83bf55d2757b5ac882f4fc395ec18c7
Emrikcah <curryandr3@gmail.com>
2023-04-05 08:22:30 -0400

    add route params to the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const Card = ({name,population,region,subregion,flags}) => {
+const Card = ({ name, population, region, subregion, flags }) => {
+   return (
+      <Link to={`/country/${name.common}`}>
+         <article className="max-w-[264px] bg-white  dark:bg-dark-mode-elements rounded-lg shadow overflow-hidden">
+            <img className="w-full h-40 object-cover " src={flags.svg} alt={`${name.common} flag`} />
+            <div className="pt-6 pb-11 pl-6 ">
+               <h2 className="font-extrabold text-light-mode-text dark:text-white text-lg pb-4">{name.common}</h2>
+               <ul className="space-y-2">
+                  <li className="font-semibold li-styles">
+                     Population: <span className="font-light li-styles">{population}</span>{" "}
+                  </li>
+                  <li className="font-semibold li-styles">
+                     Region: <span className="font-light li-styles">{region}</span>{" "}
+                  </li>
+                  <li className="font-semibold li-styles">
+                     Subregion: <span className="font-light li-styles">{subregion}</span>{" "}
+                  </li>
+               </ul>
+            </div>
+         </article>
+      </Link>
+   );
+};
 
-
-  return (
-    <article className='max-w-[264px] bg-white  dark:bg-dark-mode-elements rounded-lg shadow overflow-hidden'>
-      <img className='w-full h-40 object-cover ' src={flags.svg} alt={`${name.common} flag`} />
-      <div className='pt-6 pb-11 pl-6 '>
-        <h2 className='font-extrabold text-light-mode-text dark:text-white text-lg pb-4'>{name.common}</h2>
-        <ul className='space-y-2'>
-          <li className='font-semibold li-styles'>Population: <span className='font-light li-styles'>{population}</span> </li>
-          <li className='font-semibold li-styles'>Region: <span className='font-light li-styles'>{region}</span> </li>
-          <li className='font-semibold li-styles'>Subregion: <span className='font-light li-styles'>{subregion}</span> </li>
-        </ul>
-      </div>
-    </article>
-  )
-}
-
-export default Card
+export default Card;
