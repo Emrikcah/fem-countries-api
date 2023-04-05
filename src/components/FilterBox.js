d7cc350434d1d295af2fbc17ecb52b0194750734
Emrikcah <curryandr3@gmail.com>
2023-04-05 08:23:56 -0400

    style change to filterbox

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const FilterBox = () => {
   return (
-    <form className='rounded w-[200px]'>
-      <select name="filter-by-region" id="filter-by-region"></select>
+    <form className=''>
+      <select className='w-[200px] py-4 px-4 outline-none shadow rounded' name="filter-by-region" id="filter-by-region"></select>
     </form>
   )
 }
