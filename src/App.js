6f7ed99eafc11b3b8c6ceb7cc966bba6f0420924
Emrikcah <curryandr3@gmail.com>
2023-04-05 08:11:57 -0400

    Change route parameter: id to name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
          <Nav />
          <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="country/:id" element={<SingleCountry />} />
+            <Route path="/country/:name" element={<SingleCountry />} />
             <Route path="*" element={<Error />} />
          </Routes>
       </BrowserRouter>
