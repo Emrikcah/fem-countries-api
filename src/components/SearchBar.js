6957ee555e87e86a89c0357a11e239de5da161ec
Emrikcah <curryandr3@gmail.com>
2023-04-05 08:39:56 -0400

    add useRef to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React,{useContext,useRef} from 'react'
 import { AppContext } from "../context/Context";
 
 import { FaSearch } from "react-icons/fa";
@@ -6,14 +6,21 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
   const { setSearchTerm } = useContext(AppContext);
+  const searchValue = useRef('');
+  
+//  call this function when the user types in the input field
+  const searchCountries = () => { 
+    
+    setSearchTerm(searchValue.current.value)
+   }
 
   return (
     <form className="mb-10 md:mb-0 ">
-      <label className='relative'>
+      <label className='relative' htmlFor='search'>
         <span className='absolute inset-y-0 left-0 flex items-center pl-10 '>
           <FaSearch/>
         </span>
-        <input className="shadow appearance-none  rounded w-full md:w-[343px] py-2 px-3  leading-tight placeholder-shown:pl-20  placeholder-light-mode-input dark:placeholder-white bg-light-mode-bg dark:bg-dark-mode-elements focus:outline-none focus:shadow-outline " id="search" type="text" placeholder="Search for a country..."></input>
+        <input className="search-input" id="search" type="text" placeholder="Search for a country..." ref={searchValue} onChange={searchCountries}/>
       </label>
       
     </form>
